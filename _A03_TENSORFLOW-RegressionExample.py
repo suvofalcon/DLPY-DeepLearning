ab57320b4316605ea6080af3d8459ba0db06704f
Suvo Bhattacharya <Suvo.Bhattacharya@gmail.com>
2018-09-01 18:43:10 +0530

    Added Estimator API usage

--- _A03_TENSORFLOW-RegressionExample.py
+++ _A03_TENSORFLOW-RegressionExample.py
@@ -84,3 +84,69 @@ y_hat = x_data*model_m + model_b
 my_data.sample(250).plot(kind='scatter',x='X Data',y='Y')
 plt.plot(x_data,y_hat,'r')
 plt.show()
+
+'''
+How to use TF_ESTIMATOR
+'''
+
+# define the feature columns
+feat_cols = [tf.feature_column.numeric_column('x',shape=[1])]
+# Now we will define the estimator
+estimator = tf.estimator.LinearRegressor(feature_columns=feat_cols)
+
+# do the train, test split
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x_data,y_true,
+        test_size=0.3, random_state=101)
+
+# Check the shape of the splits
+x_train.shape
+x_test.shape
+
+# define the input function for the neural network
+input_func = tf.estimator.inputs.numpy_input_fn({'x':x_train},y_train,batch_size=10,
+                num_epochs=None,shuffle=True)
+
+train_input_func = tf.estimator.inputs.numpy_input_fn({'x':x_train},y_train,batch_size=10,
+                num_epochs=1000,shuffle=False)
+
+test_input_func = tf.estimator.inputs.numpy_input_fn({'x':x_test},y_test,batch_size=10,
+                num_epochs=None,shuffle=True)
+
+estimator.train(input_fn=input_func,steps=1000)
+
+# Now prediction and evaluation
+train_metrics = estimator.evaluate(input_fn=train_input_func,steps=1000)
+eval_metrics = estimator.evaluate(input_fn=test_input_func,steps=1000)
+
+print('Training Data Metrics')
+print(train_metrics)
+
+print('Evaluation Data Metrics')
+print(eval_metrics)
+
+'''
+This is a good way to see, whether the model is overfitting on the training data
+One of the way to check this, if the model is showing a low loss on training data and 
+high loss on the eval data... 
+Ideally loss of training and eval should be close to each other
+'''
+
+# for prediction - we will test it against a brand new data
+brand_new_data = np.linspace(0,10,10)
+input_fn_pred = tf.estimator.inputs.numpy_input_fn({'x':brand_new_data},shuffle=False)
+
+# to see the predictions we need to cast it to a list, as it returns a generator object
+# generator object - essentially a dictionary
+list(estimator.predict(input_fn=input_fn_pred))
+
+predictions = []
+for pred in estimator.predict(input_fn=input_fn_pred):
+    predictions.append(pred['predictions'])
+
+# Check the predictions
+predictions
+
+my_data.sample(n=250).plot(kind='scatter',x='X Data',y='Y')
+plt.plot(brand_new_data,predictions,'r*')
+plt.show()
